4aab9d3cb6e05f9932c0977c5d3e758851781ce2
Gregz9 <gregorkajda@gmail.com>
2023-08-13 14:11:21 +0200

    Hashing file according to docs
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- test/file_hash_test.py
+++ test/file_hash_test.py
@@ -1,6 +1,6 @@
 import hashlib
 
-data_file = open("random_data1.txt", "r")
+data_file = open("test_files/random_data1.txt", "r")
 
 print(data_file.name)
 
@@ -8,3 +8,13 @@ data = data_file.read()
 
 file_hash = hashlib.sha256(data.encode("utf-8")).hexdigest()
 print(file_hash)
+data_file.close()
+
+data2 = open("test_files/random_data1.txt", "rb")
+
+file_hash = hashlib.file_digest(data2, "sha256")
+print(file_hash.hexdigest())
+
+data2.close()
+
+data2.close()
