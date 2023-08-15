4ffa5908b1b310093935481a8d404325a5397ddf
Gregz9 <gregorkajda@gmail.com>
2023-08-15 22:52:52 +0200

    Intermediate test of multiple directory paths
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/detector.py
+++ src/detector.py
@@ -47,27 +47,37 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         directory = sys.argv[1]
     elif len(sys.argv) > 2:
-        directory = sys.argv[2:]
+        if str(sys.argv[1]) == "--sizes":
+            directory = sys.argv[2:]
+        else:
+            directory = sys.argv[1:]
 
-    paths = []
-    for dir_path, dir_names, file_names in os.walk(str(directory)):
-        for name in file_names:
-            file_path = os.path.join(dir_path, name)
-            if os.path.islink(file_path):
-                continue
-            paths.append(file_path)
+    print(directory)
+    exit()
+
+    paths = {}
+    for direct in directory:
+        p = []
+        for dir_path, dir_names, file_names in os.walk(str(directory)):
+            for name in file_names:
+                file_path = os.path.join(dir_path, name)
+                if os.path.islink(file_path):
+                    continue
+                p.append(file_path)
+        paths[direct] = p
 
     hashed_files = {}
     duplicate_files = {}
-    for path in paths:
-        data = open(path, "rb")
-        data_hash = hashlib.file_digest(data, "sha256").hexdigest()
-        file_name = data.name.replace(str(directory), "")
-        hashed_files[file_name] = [data_hash]
-        if not data_hash in duplicate_files:
-            duplicate_files[data_hash] = [file_name]
-        else:
-            duplicate_files[data_hash].append(file_name)
+    for k, v in paths.items():
+        for path in v:
+            data = open(path, "rb")
+            data_hash = hashlib.file_digest(data, "sha256").hexdigest()
+            file_name = data.name.replace(str(k), "")
+            hashed_files[file_name] = [data_hash]
+            if not data_hash in duplicate_files:
+                duplicate_files[data_hash] = [file_name]
+            else:
+                duplicate_files[data_hash].append(file_name)
 
     duplicate_files = {k: v for k, v in duplicate_files.items() if len(v) > 1}
     duplicate_copy = duplicate_files.copy()
